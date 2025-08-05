7e92191b02d73c049cb81899e3bd98cdae232cd0
MayaRosen <114875342+MayaRosen@users.noreply.github.com>
2025-08-05 16:28:37 +0300

    Update RobotController.py

--- follow_movement/RobotController.py
+++ follow_movement/RobotController.py
@@ -1,4 +1,5 @@
 import time
+import threading
 import signal
 import sys
 from unitree_sdk2py.core.channel import ChannelFactoryInitialize, ChannelSubscriber
@@ -6,13 +7,15 @@ from unitree_sdk2py.idl.unitree_go.msg.dds_ import UwbState_
 
 from uwb_state_manager import UwbStateManager
 from robot_motion_executor import RobotMotionExecutor
+from uwb_button_monitor import UwbButtonMonitor
 
 class RobotController:
     def __init__(self):
         self.state_manager = UwbStateManager()
         self.robot = None
+        self.button_monitor = UwbButtonMonitor(self.state_manager, self.signal_handler)
 
-    def signal_handler(self, signum, frame):
+    def signal_handler(self, signum=None, frame=None):
         print("Interrupt signal received.")
         if self.robot:
             self.robot.cleanup()
@@ -21,7 +24,7 @@ class RobotController:
     def run(self):
         ChannelFactoryInitialize(0)
         uwb_sub = ChannelSubscriber("rt/uwbstate", UwbState_)
-        uwb_sub.Init(self.state_manager.update_state, 10)
+        uwb_sub.Init(self.button_monitor.get_callback(), 10)
         time.sleep(1)
 
         self.robot = RobotMotionExecutor(self.state_manager)
